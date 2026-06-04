7bf9804cb0f50ef062d3167e3433f52680ba696a
De1920is <ejaranaq@uamv.edu.ni>
2026-06-04 07:55:58 -0600

    fix: select name from

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+-- ============================================================================
+-- MÓDULO I - CREACIÓN DE BASE DE DATOS (DDL)
+-- Proyecto: Hospital San Gabriel
+-- ============================================================================
+
+-- 1. Crear una base de datos llamada HospitalDB.
+CREATE DATABASE HospitalDB;
+GO
+
+-- 2. Mostrar todas las bases de datos existentes.
+SELECT name FROM sys.databases;
+GO
